13c3484c50925a6def56a4b3f79ad5ad03eb03ec
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-03-07 10:55:04 -0300

    initial commit

---
+++ codigos-exemplo/Hello-World/helloWorld_glfw.cpp
@@ -0,0 +1,63 @@
+#include <GLFW/glfw3.h>
+
+/*
+ * Programa 'Olá mundo' de OpenGL clássico.
+ * Desenha um triângulo vermelho sobre um fundo branco.
+ */
+
+
+//É uma boa prática criar uma função para agrupar configurações iniciais do OpenGL para o desenho a ser feito
+void inicio(){
+    glClearColor(1.0, 1.0, 1.0, 1.0); //indica qual cor será usada para limpar o frame buffer (normalmente usa uma cor de background)
+}
+
+
+//Função que será usada para desenhar o conteúdo no Frame Buffer
+void desenha(){
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1,3,-1,2,-1,1);
+    /*
+    glColor3f(0.0, 1.0, 1.0);
+    
+    glBegin(GL_TRIANGLES);       //instruÃ§Ã£o que indica o tipo de primitiva que serÃ¡ desenhada. Nesse caso, triÃ¢ngulos.
+        glVertex2f( 0.0,  0.0);  //instruÃ§Ã£o que envia as coordenadas dos vÃ©rtices do triÃ¢ngulo
+        glVertex2f(-1.0, -1.0);  
+        glVertex2f( 0.0, -1.0);
+    glEnd();
+    */
+    glColor3f(1.0, 1.0, 0.0);    //instruÃ§Ã£o que altera a cor corrente. glColor3f(R,G,B). R = vermelho, G = verde, B = azul
+    glBegin(GL_TRIANGLES);       //instruÃ§Ã£o que indica o tipo de primitiva que serÃ¡ desenhada. Nesse caso, triÃ¢ngulos.
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f( 0.0,  0.0);  //instruÃ§Ã£o que envia as coordenadas dos vÃ©rtices do triÃ¢ngulo
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f( 2.0,  0.0); 
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f( 1.0,  1.0);
+    glEnd();
+    
+    glFlush();  //Todas as instruÃ§Ãµes anteriores apenas indicaram o que deve ser feito. Essa Ã© a ordem pra GPU redesenhar com as informaÃ§Ãµes enviadas
+}
+
+int main(int argc, char** argv){
+    // Configuração da criação da janela com GLFW
+    glfwInit();
+
+    GLFWwindow* win = glfwCreateWindow(800, 600, "Hello World", NULL, NULL); // Criação da janela 
+    glfwMakeContextCurrent(win);
+
+    inicio(); 
+ 
+    while(!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
+        
+        glClear(GL_COLOR_BUFFER_BIT); //sempre antes de desenhar qualquer coisa, deve-se limpar o Frame Buffer
+    
+        desenha();
+
+        glfwSwapBuffers(win);
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ codigos-exemplo/Hello-World/readme.md
@@ -0,0 +1,54 @@
+# Configuração do ambiente
+
+## Python
+
+Caso não tenha as dependências do Python para desenvolvimento
+```bash
+sudo dnf (ou apt-get) install python3-devel
+```
+
+Compilador do gcc para C e C++, necessário para buildar o PyOpenGL
+```Bash
+sudo dnf (ou apt-get) install gcc gcc-c++
+```
+
+Bibliotecas do Python para comandos OpenGL
+```Bash
+pip install PyOpenGL PyOpenGL_accelerate
+```
+
+Biblioteca GLFW para criação da janela e manipulação de inputs de teclado e mouse
+```Bash
+sudo dnf (ou apt-get) install glfw-devel
+```
+
+Então para testar basta executar o HelloWorld e você deve ver um triângulo na tela.
+```Bash
+python helloWorld_glfw.py
+```
+
+## C++
+
+Para C++ vamos utilizar CMake, cmake vai facilitar o processo de build, mais detalhes no arquivo CMakeLists.txt.
+
+Para baixar o CMake. (Pelo Windows basta baixar o instalador do site oficial)
+```Bash
+sudo apt-get install cmake
+```
+
+Para então buildar o projeto, tem que estar no diretório que se encontra o CMakeLists.txt
+```Bash
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+
+### Sobre os comandos acima
+Você está criando uma pasta de build, passando para ela e executando ```cmake ..``` para gerar os arquivos de build do cmake nesta pasta, ```cmake --build .``` vai então gerar seu executável nesta pasta.
+ 
+E então pode executar o arquivo final
+```Bash
+./Hello
+```
+
